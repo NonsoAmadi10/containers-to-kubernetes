c076b123860424dcabb05e317c6587f211374d53
Houdini <nonsoamadi@aol.com>
2020-11-12 00:33:22 +0100

    setup docker compose for dev

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:10.16-alpine
 WORKDIR /usr/src/app
 COPY ["package.json", "yarn.lock*", "./"]
-RUN yarn install --production --silent 
+RUN yarn install --production --silent && yarn global add nodemon 
 COPY . .
 CMD ["yarn", "start"]
 EXPOSE 8080/tcp
--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const router = express.Router();
 const path = __dirname + '/views/';
-const port = 8080;
+const port = process.env.PORT || 8080;
 const sharks = require('./routes');
 
 dotenv.config();
@@ -13,7 +13,16 @@ const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOSTNAME, MONGO_PORT, MONGO_DB } =
 const url =
     `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const options = {
+    useNewUrlParser: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 500,
+    connectTimeoutMS: 10000,
+};
+
+
+mongoose.connect(url, options)
     .then(() => console.log('connection suceeded'))
     .catch((err) => console.log(err));
 app.use(express.urlencoded({ extended: false }));
@@ -27,5 +36,5 @@ app.set('views', path);
 
 app.use('/', router);
 app.listen(port, function () {
-    console.log('web app running on port 8080!')
+    console.log(`web app running on ${port}`)
 })
