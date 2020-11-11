d54215a7cff8023f04383dea59441d67f31c67aa
Houdini <nonsoamadi@aol.com>
2020-11-11 01:41:03 +0100

    connect to mongodb

--- src/app.js
+++ src/app.js
@@ -7,6 +7,14 @@ const path = __dirname + '/views/';
 const port = 8080;
 
 dotenv.config();
+const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOSTNAME, MONGO_PORT, MONGO_DB } = process.env
+
+const url =
+    `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('connection suceeded'))
+    .catch((err) => console.log(err));
 
 router.use(function (req, res, next) {
     console.log('/' + req.method);
